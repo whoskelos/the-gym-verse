330ffd5f2ff18cd81691d727edd5e35fdce4a385
whoskelos ubuntu <kelvinguerrero2199@gmail.com>
2023-06-02 17:00:58 +0200

    vista-detalle-musculo.component.ts ya realiza el find del ejercicio seleccionado, pinto en la vista el titulo y el subtitulo

--- src/app/components/vista-detalle-musculo/vista-detalle-musculo.component.ts
+++ src/app/components/vista-detalle-musculo/vista-detalle-musculo.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IEjercicio } from 'src/app/models/ejercicio.interface';
+import { EJERCICIOS } from 'src/app/mocks/ejercicios.mock';
 
 @Component({
   selector: 'app-vista-detalle-musculo',
   templateUrl: './vista-detalle-musculo.component.html',
   styleUrls: ['./vista-detalle-musculo.component.css']
 })
-export class VistaDetalleMusculoComponent {
+export class VistaDetalleMusculoComponent implements OnInit{
+  
+  id: string | null = '';
+  ejercicioSeleccionado: IEjercicio | undefined;
+  listaEjercicios: IEjercicio[] = EJERCICIOS;
+  
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    
+    // * Obtengo el id del ejercicio seleccionado que viene por queryParam para buscarlo
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id != '') {
+      this.ejercicioSeleccionado = this.listaEjercicios.find((ejercicio) => ejercicio.id == this.id);
+      console.log(this.ejercicioSeleccionado);
+    }
+  }
 
 }
